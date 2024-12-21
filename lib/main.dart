7fae5edaf3e501d1598831f5505d85b613de0453
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-21 15:51:49 +0300

    delegates and supported locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_localization/router/custom_router.dart';
 import 'package:simple_localization/router/route_constants.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +27,17 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('fa'), // Farsi
+        Locale('ar'), // Arabic
+      ],
+      locale: const Locale('ar', ''),
       // TODO: implement localizations
       onGenerateRoute: CustomRouter.generatedRoute,
       initialRoute: homeRoute,
