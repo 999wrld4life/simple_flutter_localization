0e898610cc969d732abfb2129506ac0b3332d270
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-21 23:30:42 +0300

    shared preferences setup for persistence

---
+++ lib/classes/language.constants.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String LANGUAGE_CODE = 'languageCode';
+
+const String ENGLISH = 'en';
+const String FARSI = 'fa';
+const String ARABIC = 'ar';
+
+Future<Locale> setLocale(String languageCode) async {
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  await _prefs.setString(LANGUAGE_CODE, languageCode);
+  return _locale(languageCode);
+}
+
+Future<Locale> getLocale() async {
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  String languageCode = _prefs.getString(LANGUAGE_CODE) ?? ENGLISH;
+  return _locale(languageCode);
+}
+
+Locale _locale(String languageCode) {
+  switch (languageCode) {
+    case ENGLISH:
+      return const Locale(ENGLISH, 'US');
+    case FARSI:
+      return const Locale(FARSI, 'IR');
+    case ARABIC:
+      return const Locale(ARABIC, 'SA');
+    default:
+      return const Locale(ENGLISH, 'US');
+  }
+}
+
+AppLocalizations translation(BuildContext context) {
+  return AppLocalizations.of(context)!;
+}
