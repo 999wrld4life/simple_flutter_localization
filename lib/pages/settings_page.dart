e084f6adc3c037395739b9c7601005f5a5b7ef66
Nathaniel Abayneh <natnaelabayneh5@gmail.com>
2024-12-22 01:34:47 +0300

    settings page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:simple_localization/classes/language.constants.dart';
+import 'package:simple_localization/classes/language.dart';
+import 'package:simple_localization/main.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -12,9 +17,37 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Settings"),
+        title: Text(translation(context).settings),
       ),
-      body: const Center(child: Text("Settings Page")),
+      body: Center(
+          child: DropdownButton<Language>(
+        iconSize: 30,
+        hint: Text(translation(context).changeLanguage),
+        onChanged: (Language? language) async {
+          if (language != null) {
+            // ignore: no_leading_underscores_for_local_identifiers
+            Locale _locale = await setLocale(language.languageCode);
+            MyApp.setLocale(context, _locale);
+          }
+        },
+        items: Language.languageList()
+            .map<DropdownMenuItem<Language>>(
+              (e) => DropdownMenuItem<Language>(
+                value: e,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      e.flag,
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                    Text(e.name)
+                  ],
+                ),
+              ),
+            )
+            .toList(),
+      )),
     );
   }
 }
